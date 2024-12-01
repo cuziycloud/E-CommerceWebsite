2d7b387744dc1eaa62c2c0f2ba9225b6fb79a359
hiddcten <xuantruong3204.vietnam@gmail.com>
2024-12-01 19:41:16 +0700

    add edit product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,8 @@ import ForgotPassword from './components/ForgotPassword';
 import UserProfileManagement from './components/UserProfileManagement';
 import NotFound from './components/NotFound';
 import AddProductForm from './components/AddProductForm';
+import ProductEditPage from './components/ProductEditPage';
+
 import './styles/tailwind.css';
 
 function App() {
@@ -25,6 +27,7 @@ function App() {
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/user-profile" element={<UserProfileManagement />} />
           <Route path="/admin/add-product" element={<AddProductForm/>}/>
+          <Route path="/admin/edit-product" element={<ProductEditPage/>}/>
           {/* Route dành cho các đường dẫn không tồn tại */}
           <Route path="*" element={<NotFound />} />
         </Routes>
